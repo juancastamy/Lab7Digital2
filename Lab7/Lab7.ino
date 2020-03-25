ece312866fd210f0aeb784cbda1da560020a32f8
juancastamy <45299603+juancastamy@users.noreply.github.com>
2020-03-25 17:34:38 -0600

    Inicializacion
    Se inicializo correctamente la SD y se realizo la primera programacion para desplegar una imagen

--- Lab7/Lab7.ino
+++ Lab7/Lab7.ino
@@ -1,6 +1,6 @@
 /*  /*
  * Seven lab for Digital 2 course  
- * File:   lab7
+ * File:   lab7.ino
  * Author: Juan Castillo
  * Student ID: 17074
  *
@@ -32,42 +32,49 @@
 #include <SD.h>
 
 File myFile;
-
+int x = 0;
 void setup() {
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
+
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
-  SPI.setModule(0);
-  
+ 
 
   Serial.print("Initializing SD card...");
-
   if (!SD.begin(4)) {
     Serial.println("initialization failed!");
     while (1);
   }
   Serial.println("initialization done.");
+  
 
- 
-  // re-open the file for reading:
-  myFile = SD.open("test.txt");
-  if (myFile) {
-    Serial.println("test.txt:");
 
-    // read from the file until there's nothing else in it:
-    while (myFile.available()) {
-      Serial.write(myFile.read());
-    }
-    // close the file:
-    myFile.close();
-  } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
-  }
 }
 
 void loop() {
+  if(Serial.available()>0){
+  x = Serial.read();
+  Serial.println(x);
+  }
+  if(x==77){
+    // re-open the file for reading:
+    myFile = SD.open("METROID.txt");
+    if (myFile) {
+      Serial.println("Archivo:");
+
+    // read from the file until there's nothing else in it:
+      while (myFile.available()) {
+        Serial.write(myFile.read());
+    }
+    // close the file:
+    myFile.close();
+    } 
+    else {
+    // if the file didn't open, print an error:
+      Serial.println("error opening test.txt");
+  }
+  }
   // nothing happens after setup
 }
